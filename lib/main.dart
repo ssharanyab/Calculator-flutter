984ddb9d3c76cb13424e4788942918829ff121b4
Sharanya-Bharghavi <ssharanyab@gmail.com>
2020-05-08 16:35:18 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -6,17 +8,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My Calculator',
       theme: ThemeData(
-        primarySwatch: Colors.lightGreen,
+        primarySwatch: Colors.cyan,
       ),
-      home: MyHomePage(title: 'My Calculator'),
+      home: MyHomePage(title: 'Calculator- Made by Sharanya'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}):super(key: key);
+  MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
   @override
@@ -24,6 +26,73 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String output = "0";
+  String _output = "0";
+  double n1 = 0.0;
+  double n2 = 0.0;
+  String operand = "";
+
+  buttonPressed(String buttonText) {
+    if (buttonText == "Clear") {
+      _output = "0";
+      n1 = 0.0;
+      n2 = 0.0;
+      operand = "";
+    } else if (buttonText == "+" ||
+        buttonText == "-" ||
+        buttonText == "x" ||
+        buttonText == "/") {
+      n1 = double.parse(output);
+      operand = buttonText;
+      _output = "0";
+    } else if (buttonText == ".") {
+      if (_output.contains(".")) {
+        print("Already contains a decimal");
+        return;
+      } else {
+        _output = _output + buttonText;
+      }
+    } else if (buttonText == "=") {
+      n2 = double.parse(output);
+      if (operand == "+") {
+        _output = (n1 + n2).toString();
+      }
+      if (operand == "-") {
+        _output = (n1 - n2).toString();
+      }
+      if (operand == "x") {
+        _output = (n1 * n2).toString();
+      }
+      if (operand == "/") {
+        _output = (n1 / n2).toString();
+      }
+      n1 = 0.0;
+      n2 = 0.0;
+      operand = "";
+    } else {
+      _output = _output + buttonText;
+    }
+    print(_output);
+    setState(() {
+      output = double.parse(_output).toStringAsFixed(2);
+    });
+  }
+
+  Widget button(String buttontext) {
+    return new Expanded(
+      child: new MaterialButton(
+        padding: new EdgeInsets.all(27),
+        shape: new RoundedRectangleBorder(),
+        child: new Text(
+          buttontext,
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        color: Colors.lime,
+        onPressed: () => {buttonPressed(buttontext)},
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,12 +100,52 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: new Container(
-        child: new Column(children:<Widget>[
-          new Text("0")
-
-        ],)
-     
-      ),
+          child: new Column(
+        children: <Widget>[
+          new Container(
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
+              child: new Text(
+                output,
+                style: new TextStyle(
+                  fontSize: 48.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              )),
+          new Expanded(
+            child: new Divider(),
+          ),
+          new Column(
+            children: [
+              new Row(children: [
+                button("7"),
+                button("8"),
+                button("9"),
+                button("+")
+              ]),
+              new Row(children: [
+                button("4"),
+                button("5"),
+                button("6"),
+                button("/")
+              ]),
+              new Row(children: [
+                button("1"),
+                button("2"),
+                button("3"),
+                button("x")
+              ]),
+              new Row(children: [
+                button("0"),
+                button("00"),
+                button("."),
+                button("-")
+              ]),
+              new Row(children: [button("Clear"), button("=")]),
+            ],
+          ),
+        ],
+      )),
     );
   }
 }
